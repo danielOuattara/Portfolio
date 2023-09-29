fb4c41065c24dca7d7a40f0921a61efa922504c4
danielOuattara <daniel.ouattara@gmx.com>
2023-09-29 22:41:59 +0200

    contact page

---
+++ src/pages/contact.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import { Seo } from "../components";
+
+//-----------------------------------------------------------------------
+
+export default function Contact() {
+  return (
+    <Seo>
+      <section className="contact-page">
+        <article className="contact-form">
+          <h3>stay in touch ?</h3>
+          <form
+            className="form contact-form"
+            action="https://formspree.io/f/mdorpabo"
+            method="POST"
+          >
+            <div className="form-group">
+              <label htmlFor="name">
+                <input
+                  type="text"
+                  name="name"
+                  id="name"
+                  placeholder="name"
+                  className="form-control"
+                />
+              </label>
+              <label htmlFor="email">
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  placeholder="email"
+                  className="form-control"
+                />
+              </label>
+              <label htmlFor="message">
+                <textarea
+                  name="message"
+                  id="message"
+                  rows="7"
+                  className="form-control"
+                  placeholder="your message"
+                ></textarea>
+              </label>
+            </div>
+            <button type="submit" className="submit-btn btn">
+              submit
+            </button>
+          </form>
+        </article>
+      </section>
+    </Seo>
+  );
+}
+
+//-----------------------------------------------------------------------
+
+export const Head = () => <Seo title={"Contacts"} />;
