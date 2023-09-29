ce555010b844addf4525da9b9926fe8f3ad6517e
danielOuattara <daniel.ouattara@gmx.com>
2023-09-29 09:23:17 +0200

    Services

---
+++ src/components/Services.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Title } from "./index";
+import { services } from "../constants";
+
+export default function Services() {
+  return (
+    <section className="section bg-grey">
+      <Title title="Services" />
+      <div className="section-center services-center">
+        {services.map((service) => (
+          <article key={service.id} className="service">
+            {service.icon}
+            <h4>{service.title}</h4>
+            <div className="underline"></div>
+            <p>{service.text}</p>
+          </article>
+        ))}
+      </div>
+    </section>
+  );
+}
