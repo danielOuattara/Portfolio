67979d881cabfc7fecfa004a79898544a138dafe
danielOuattara <daniel.ouattara@gmx.com>
2023-10-03 10:19:03 +0200

    single project template

---
+++ src/pages/projects/{ContentfulPortfolioProjects.title}.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { Link, graphql } from "gatsby";
+import { Seo } from "./../../components";
+
+//-------------------------------------------------------------------------------
+
+export default function SingleProjectTemplate(props) {
+  //   console.log(props);
+  const projectData = props.data.contentfulPortfolioProjects;
+  console.log(projectData.featured_image.publicUrl);
+  return (
+    <main className="project-template-page">
+      <h2>{props.pageContext.title}</h2>
+      <p>{projectData.description.description}</p>
+      <a href={projectData.url} target="_blank" rel="noreferrer">
+        <img
+          src={projectData.featured_image.publicUrl}
+          alt={projectData.title}
+        />
+      </a>
+      <Link to="/projects" className="btn">
+        back to projects
+      </Link>
+    </main>
+  );
+}
+
+//-------------------------------------------------------------------------------
+export const query = graphql`
+  query SingleProjectQuery($title: String) {
+    contentfulPortfolioProjects(title: { eq: $title }) {
+      id
+      title
+      grade
+      category
+      level
+      description {
+        description
+      }
+      url_website
+      url_github
+      featured
+      featured_image {
+        id
+        gatsbyImageData(layout: CONSTRAINED, placeholder: BLURRED)
+        publicUrl
+      }
+      technologies
+      images_list {
+        gatsbyImageData(layout: CONSTRAINED, placeholder: BLURRED)
+        id
+        publicUrl
+      }
+    }
+  }
+`;
