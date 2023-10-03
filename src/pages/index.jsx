a96ff438e66b7120fac5b83fe21cdfca246a0f5a
danielOuattara <daniel.ouattara@gmx.com>
2023-10-03 10:21:29 +0200

    index page update

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,12 +1,47 @@
 import React from "react";
-import { Hero } from "../components";
+import { Hero, Services /* Jobs */, Projects } from "../components";
+import { graphql } from "gatsby";
 
-export default function Home() {
+export default function Home(props) {
+  const featuredProjects = props.data.allContentfulPortfolioProjects.nodes;
+
   return (
     <>
       <main>
         <Hero />
+        <Services />
+        {/* <Jobs /> */}
+        <Projects
+          title="featured projects"
+          showLinkToProjects
+          projects={featuredProjects}
+        />
       </main>
     </>
   );
 }
+
+export const query = graphql`
+  query {
+    allContentfulPortfolioProjects(filter: { featured: { eq: true } }) {
+      totalCount
+      nodes {
+        id
+        title
+        grade
+        category
+        level
+        description {
+          description
+        }
+        url_website
+        url_github
+        featured
+        featured_image {
+          gatsbyImageData(layout: CONSTRAINED, placeholder: BLURRED)
+        }
+        technologies
+      }
+    }
+  }
+`;
