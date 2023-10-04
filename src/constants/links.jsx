11e33d3f92ce6df909eb3e6feeed7ea415c860b2
danielOuattara <daniel.ouattara@gmx.com>
2023-10-04 23:03:07 +0200

    updating subLinks

--- src/constants/links.jsx
+++ src/constants/links.jsx
@@ -1,28 +1,38 @@
 const page_links = [
   {
     id: 1,
-    text: "home",
     url: "/",
+    page: "home",
   },
   {
     id: 2,
-    text: "about",
-    url: "/about/",
+    url: "/about",
+    page: "about",
   },
   {
     id: 3,
-    text: "projects",
-    url: "/projects/",
+    url: "/projects",
+    page: "projects",
+    subLinks: [
+      { id: "3-1", url: "/projects/frontend", page: "frontend" },
+      { id: "3-2", url: "/projects/backend", page: "backend" },
+      { id: "3-3", url: "/projects/fullstack", page: "fullstack" },
+    ],
   },
   {
     id: 4,
-    text: "documents",
-    url: "/documents/",
+    url: "/documents",
+    page: "documents",
+    subLinks: [
+      { id: "4-1", url: "/documents/cv", page: "cv" },
+      { id: "4-2", url: "/documents/diploma", page: "diploma" },
+      { id: "4-1", url: "/documents/certificates", page: "certificates" },
+    ],
   },
   {
     id: 5,
-    text: "contact",
-    url: "/contact/",
+    page: "contact",
+    url: "/contact",
   },
 ];
 
