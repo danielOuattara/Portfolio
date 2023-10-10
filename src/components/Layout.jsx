f1f9093d558ed4c75293a028b43a2bfdf97ec6db
danielOuattara <daniel.ouattara@gmx.com>
2023-10-10 21:40:19 +0200

    update Layout to handle displaying Submenu

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,17 +1,45 @@
 import React, { useState } from "react";
-import { Navbar, Sidebar, Footer } from "./";
+import { Navbar, Sidebar, Footer, Submenu } from "./";
 import "../assets/css/main.css";
 
 export default function Layout(props) {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
-  const toggleSidebar = () => {
-    setIsSidebarOpen(!isSidebarOpen);
-  };
+  /* ------------------------------------- */
+
+  const [isSubMenuOpen, setIsSubMenuOpen] = useState(false);
+  const [subMenuLocation, setSubMenuLocation] = useState({
+    subMenuCenterPosition: 0,
+    subMenuTopPosition: 0,
+  });
+  const [subMenuPageToShown, setSubMenuPageToShown] = useState({
+    id: "",
+    page: "",
+    subLinks: [],
+    url: "",
+  });
+  // console.log("subMenuPageToShown = ", subMenuPageToShown);
+
+  /* ------------------------------------- */
+
+  const toggleSidebar = () => setIsSidebarOpen(!isSidebarOpen);
+
   return (
     <>
-      <Navbar toggleSidebar={toggleSidebar} />
+      <Navbar
+        toggleSidebar={toggleSidebar}
+        // setIsSidebarOpen={setIsSidebarOpen}
+        setIsSubMenuOpen={setIsSubMenuOpen}
+        setSubMenuLocation={setSubMenuLocation}
+        setSubMenuPageToShown={setSubMenuPageToShown}
+      />
       <Sidebar toggleSidebar={toggleSidebar} isSidebarOpen={isSidebarOpen} />
+      <Submenu
+        isSubMenuOpen={isSubMenuOpen}
+        subMenuLocation={subMenuLocation}
+        subMenuPageToShown={subMenuPageToShown}
+        setIsSubMenuOpen={setIsSubMenuOpen}
+      />
       {props.children}
       <Footer />
     </>
