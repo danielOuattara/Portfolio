38cea9eb5740f4d1c9a6e45f6bddaa80356670d6
danielOuattara <daniel.ouattara@gmx.com>
2023-10-10 21:38:49 +0200

    Submenu component

---
+++ src/components/Submenu.jsx
@@ -0,0 +1,38 @@
+import React, { useState, useRef, useEffect } from "react";
+import { Link } from "gatsby";
+//--------------------------------------------------------------
+
+export default function Submenu(props) {
+  // console.log("props = ", props);
+  const [columns, setColumns] = useState("");
+  const subMenuContainerRef = useRef(null);
+
+  useEffect(() => {
+    props.subMenuPageToShown.subLinks.length <= 3
+      ? setColumns("col-3")
+      : setColumns("col-4");
+    subMenuContainerRef.current.style.left = `${props.subMenuLocation.subMenuCenterPosition}px`;
+    subMenuContainerRef.current.style.top = `${props.subMenuLocation.subMenuTopPosition}px`;
+  }, [props.subMenuLocation, props.subMenuPageToShown.subLinks]);
+
+  const hideSubMenu = () => {
+    props.setIsSubMenuOpen(false);
+  };
+  return (
+    <aside
+      className={props.isSubMenuOpen ? "submenu show" : "submenu"}
+      ref={subMenuContainerRef}
+      onMouseLeave={() => hideSubMenu()}
+    >
+      <div className={` nav-subLinks submenu-center ${columns} `}>
+        {props.subMenuPageToShown.subLinks.map((subLink, index) => {
+          return (
+            <Link key={index} to={subLink.url} className="link-btn">
+              {subLink.page}
+            </Link>
+          );
+        })}
+      </div>
+    </aside>
+  );
+}
