81aba010e0dbb9de22c8db9531fb42354cbb6443
danielOuattara <daniel.ouattara@gmx.com>
2023-12-12 19:55:46 +0100

    portfolio-gatsby: updated projects routes logic

---
+++ src/pages/projects/mobile/index.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { graphql } from "gatsby";
+import { Projects, Seo } from "../../../components";
+
+export default function FrontendProjectsPage(props) {
+  console.log("props = ", props);
+  const projects = props.data.allContentfulPortfolioProjects.nodes;
+  return (
+    <Seo>
+      <main>
+        <section className="projects-page">
+          <Projects
+            title="mobile projects"
+            projects={projects}
+            gradePath={"mobile"}
+          />
+        </section>
+      </main>
+    </Seo>
+  );
+}
+
+export const Head = () => <Seo title={"Mobile Projects"} />;
+
+export const query = graphql`
+  query allProjectByGrade {
+    allContentfulPortfolioProjects(filter: { type: { eq: "mobile" } }) {
+      totalCount
+      nodes {
+        id
+        title
+        type
+        level
+        description {
+          description
+        }
+        url_website
+        url_github
+        featured
+        featured_image {
+          id
+          gatsbyImageData(layout: CONSTRAINED, placeholder: BLURRED)
+          publicUrl
+        }
+        technologies
+        images_list {
+          gatsbyImageData(layout: CONSTRAINED, placeholder: BLURRED)
+          id
+          publicUrl
+        }
+      }
+    }
+  }
+`;
